a8b6a9fcf3fdc24b9bbd7167272f373414197ca2
sbx_user1051 <sbx_user1051@169.254.66.179>
2025-05-18 18:06:03 +0000

    Sun May 18 18:06:03 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,111 +1,117 @@
+prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+    'E': 40,
+    'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50
+}
+
+offers = [
+    {
+        'item': 'A',
+        'required': 3,
+        'discounted_price': 130
+    },
+    {
+        'item': 'A',
+        'required': 5,
+        'discounted_price': 200
+    },
+    {
+        'item': 'B',
+        'required': 2,
+        'discounted_price': 45
+    },
+    {
+        'item': 'E',
+        'required': 2,
+        'free_item': 'B'
+    },
+    {
+        'item': 'F',
+        'required': 2,
+        'free_item': 'F'
+    },
+    {
+        'item': 'H',
+        'required': 5,
+        'discounted_price': 45
+    },
+    {
+        'item': 'H',
+        'required': 10,
+        'discounted_price': 80
+    },
+    {
+        'item': 'K',
+        'required': 2,
+        'discounted_price': 150
+    },
+    {
+        'item': 'N',
+        'required': 3,
+        'free_item': 'M'
+    },
+    {
+        'item': 'P',
+        'required': 5,
+        'discounted_price': 200
+    },
+    {
+        'item': 'Q',
+        'required': 3,
+        'discounted_price': 80
+    },
+    {
+        'item': 'R',
+        'required': 3,
+        'free_item': 'Q'
+    },
+    {
+        'item': 'U',
+        'required': 3,
+        'free_item': 'U'
+    },
+    {
+        'item': 'V',
+        'required': 2,
+        'discounted_price': 90
+    },
+    {
+        'item': 'V',
+        'required': 3,
+        'discounted_price': 130
+    },
+]
+
 
 class CheckoutSolution:
-    prices = {
-        'A': 50,
-        'B': 30,
-        'C': 20,
-        'D': 15,
-        'E': 40,
-        'F': 10,
-        'G': 20,
-        'H': 10,
-        'I': 35,
-        'J': 60,
-        'K': 80,
-        'L': 90,
-        'M': 15,
-        'N': 40,
-        'O': 10,
-        'P': 50,
-        'Q': 30,
-        'R': 50,
-        'S': 30,
-        'T': 20,
-        'U': 40,
-        'V': 50,
-        'W': 20,
-        'X': 90,
-        'Y': 10,
-        'Z': 50
-    }
 
-    offers = [
-        {
-            'item': 'A',
-            'required': 3,
-            'discounted_price': 130
-        },
-        {
-            'item': 'A',
-            'required': 5,
-            'discounted_price': 200
-        },
-        {
-            'item': 'B',
-            'required': 2,
-            'discounted_price': 45
-        },
-        {
-            'item': 'E',
-            'required': 2,
-            'free_item': 'B'
-        },
-        {
-            'item': 'F',
-            'required': 2,
-            'free_item': 'F'
-        },
-        {
-            'item': 'H',
-            'required': 5,
-            'discounted_price': 45
-        },
-        {
-            'item': 'H',
-            'required': 10,
-            'discounted_price': 80
-        },
-        {
-            'item': 'K',
-            'required': 2,
-            'discounted_price': 150
-        },
-        {
-            'item': 'N',
-            'required': 3,
-            'free_item': 'M'
-        },
-        {
-            'item': 'P',
-            'required': 5,
-            'discounted_price': 200
-        },
-        {
-            'item': 'Q',
-            'required': 3,
-            'discounted_price': 80
-        },
-        {
-            'item': 'R',
-            'required': 3,
-            'free_item': 'Q'
-        },
-        {
-            'item': 'U',
-            'required': 3,
-            'free_item': 'U'
-        },
-        {
-            'item': 'V',
-            'required': 2,
-            'discounted_price': 90
-        },
-        {
-            'item': 'V',
-            'required': 3,
-            'discounted_price': 130
-        },
-    ]
+    total: int = 0
+
+    class Item:
+        count: int
 
     def offers_sorted_by_required(self, input_offers: list[dict]) -> list[dict]:
         return sorted(input_offers, key=lambda x: x['required'], reverse=True)
@@ -123,6 +129,9 @@ class CheckoutSolution:
                 total += div * discount['price']
                 rem_skus = rem_skus % discount['count']
         return total + rem_skus * self.prices[sku]
+    
+    def apply_discount(self, item, offer):
+        pass
                 
 
     def total_sku(self, skus, sku) -> int:
@@ -148,9 +157,10 @@ class CheckoutSolution:
                 return -1                
         return total
 
-client = CheckoutSolution()
-offers = client.remove_unrelated_offers('ABCDH')
-print(client.offers_sorted_by_required(offers))
+# client = CheckoutSolution()
+# offers = client.remove_unrelated_offers('ABCDH')
+# print(client.offers_sorted_by_required(offers))
+
 
 
 
